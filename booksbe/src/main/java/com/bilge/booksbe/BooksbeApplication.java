1e97900f46987857dd74b5348517a93a72936edd
bilge <bilgeerdem7@outlook.com>
2022-09-03 17:06:19 +0300

    no message

---
+++ booksbe/src/main/java/com/bilge/booksbe/BooksbeApplication.java
@@ -0,0 +1,13 @@
+package com.bilge.booksbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksbeApplication.class, args);
+	}
+
+}
---
+++ booksbe/src/test/java/com/bilge/booksbe/BooksbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilge.booksbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
